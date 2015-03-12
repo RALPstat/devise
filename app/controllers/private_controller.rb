2e6ac0736a380833c0d1838d2ddc14b86a85e265
RALPstat <ralp.stat@gmail.com>
2015-03-12 11:01:14 -0500

    Añadida autenticación a los modelos

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -1,4 +1,10 @@
 class PrivateController < ApplicationController
   def index
+  	if user_signed_in?
+  		self  		
+  	else
+  		flash[:alert] = "Oops! Debes iniciar sesión antes de ver esto"
+  		redirect_to public_index_path
+  	end
   end
 end
--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,4 +1,7 @@
 class PublicController < ApplicationController
   def index
+  	if user_signed_in?
+  		redirect_to private_index_path
+  	end
   end
 end
